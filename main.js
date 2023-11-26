370d166a669ca26c7815004209416cb2be16359f
juancruza46 <juan.cruza46@gmail.com>
2023-11-25 21:03:03 -0500

    added css style & JS event listeners

--- main.js
+++ main.js
@@ -16,14 +16,22 @@ const winningCombonations = [
     [0, 4, 8],
     [2, 4, 6]
   ];
-  
+
 // Define required variables used to track the state of the game
+let board;
+let playersTurn;
+let winner;
 
 // Store elements on the page that will be accessed in code more than once in variables to make code more concise, readable and performant.
+const text = document.querySelector('h1');
+const buttonNewGame = document.querySelector('button');
 
-// Upon loading the app should:
+// Upon loading the app should wait for users interaction
+document.getElementById('gameBoard').addEventListener('click', handleMove);
+buttonNewGame.addEventListener('click', begin);
   
 
 // Handle a player clicking a square
+
 
 // Handle a player clicking the replay button

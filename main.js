3119f56b200c172a2bcd834b98facd4fec77ab71
juancruza46 <juan.cruza46@gmail.com>
2023-11-25 20:35:01 -0500

    Colors + winning combinations updated

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 // Define required constants
+const colors  = {
+    '1': 'purple' ,
+    '-1': 'pink', 
+    'null': 'white'
+};
 
+//Possible winning combinations: vertical, horizontal, diagonal
+const winningCombonations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  
 // Define required variables used to track the state of the game
 
 // Store elements on the page that will be accessed in code more than once in variables to make code more concise, readable and performant.

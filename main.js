bf63af601c5460fbc0e10639c948cf54276dffd5
juancruza46 <juan.cruza46@gmail.com>
2023-11-25 21:25:50 -0500

    update states + render function + receive results

--- main.js
+++ main.js
@@ -27,8 +27,45 @@ const text = document.querySelector('h1');
 const buttonNewGame = document.querySelector('button');
 
 // Upon loading the app should wait for users interaction
-document.getElementById('gameBoard').addEventListener('click', handleMove);
+document.getElementById('board').addEventListener('click', handleMove);
 buttonNewGame.addEventListener('click', begin);
+
+//function post clicked
+begin();
+
+function begin() {
+    board = new Array(9).fill(null);
+    turn = 1;
+    winner = null;
+    render();
+}
+
+//Update variables interacted with
+function handleMove(evt) {
+    const idx = parseInt(evt.target.id.replace('sq-', ''));
+    if (
+        isNaN(idx) ||
+        board[idx] ||
+        winner
+    ) return;
+
+//Update state + render
+board[idx]= playersTurn;
+playersTurn *= -1;
+winner = getWinner();
+render();
+}
+//Render results
+
+function getWinner () {
+    for (let winArr of winningCombonations){
+        if(Math.abs(board[winArr[0]] + board[winArr[1]] + board[winArr[2]]) === 3)
+        return board[winArr[0]];
+    }
+
+    if (board.includes(null)) return null;
+    return 'T';
+}
   
 
 // Handle a player clicking a square

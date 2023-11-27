3098964ef11204355c58006069c1a1d6b653c884
juancruza46 <juan.cruza46@gmail.com>
2023-11-26 19:30:08 -0500

    html complete, js complete, css style complete

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-// Define required constants
-const colors  = {
+//Define required constants
+const colors = {
     '1': 'purple' ,
-    '-1': 'pink', 
+   '-1': 'pink',
     'null': 'white'
-};
-
-//Possible winning combinations: vertical, horizontal, diagonal
-const winningCombonations = [
+  };
+  
+  let board;
+  let playersTurn;
+  let winner;
+   
+  //Possible winning combinations: vertical, horizontal, diagonal
+  const possibleCombonations = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -16,59 +20,71 @@ const winningCombonations = [
     [0, 4, 8],
     [2, 4, 6]
   ];
+  
+  //Store elements on the page that will be accessed in code more than once in variables to make code more concise, readable and performant.
+  const text = document.querySelector('h1');
+  const playButton = document.querySelector('button');
 
-// Define required variables used to track the state of the game
-let board;
-let playersTurn;
-let winner;
-
-// Store elements on the page that will be accessed in code more than once in variables to make code more concise, readable and performant.
-const text = document.querySelector('h1');
-const buttonNewGame = document.querySelector('button');
-
-// Upon loading the app should wait for users interaction
-document.getElementById('board').addEventListener('click', handleMove);
-buttonNewGame.addEventListener('click', begin);
-
-//function post clicked
-begin();
-
-function begin() {
+  //Event Listeners
+  document.getElementById('board').addEventListener('click', handleMove);
+  playButton.addEventListener('click', initialize);
+  
+  //Add functions to recieve game results
+  initialize();
+  
+  function initialize() {
     board = new Array(9).fill(null);
-    turn = 1;
+    playersTurn = 1;
     winner = null;
     render();
-}
-
-//Update variables interacted with
-function handleMove(evt) {
+  }
+  
+  //Update variables interacted with
+  function handleMove(evt) {
     const idx = parseInt(evt.target.id.replace('sq-', ''));
     if (
-        isNaN(idx) ||
-        board[idx] ||
-        winner
+      isNaN(idx) ||
+      board[idx] ||
+      winner
     ) return;
-
-//Update state + render
-board[idx]= playersTurn;
-playersTurn *= -1;
-winner = getWinner();
-render();
-}
-//Render results
-
-function getWinner () {
-    for (let winArr of winningCombonations){
-        if(Math.abs(board[winArr[0]] + board[winArr[1]] + board[winArr[2]]) === 3)
-        return board[winArr[0]];
+   
+    //Update state + render
+    board[idx] = playersTurn;
+    playersTurn *= -1;
+    winner = winnerResults();
+    render();
+  }
+  //Render results
+  function winnerResults() {
+    for (let winArr of possibleCombonations) {
+      if (Math.abs(board[winArr[0]] + board[winArr[1]] + board[winArr[2]]) === 3) return board[winArr[0]];
     }
-
     if (board.includes(null)) return null;
     return 'T';
-}
+  }
   
-
-// Handle a player clicking a square
-
-
-// Handle a player clicking the replay button
+  // Visualize all state and info in the DOM
+  function render() {
+    renderBoard();
+    renderText();
+    playButton.disabled = !winner;
+  }
+  
+  function renderBoard() {
+    board.forEach(function(sqVal, idx) {
+      const squareEl = document.getElementById(`sq-${idx}`);
+      squareEl.style.backgroundColor = colors[sqVal];
+      squareEl.className = !sqVal ? 'avail' : '';
+    });
+  }
+  
+  function renderText() {
+    if (winner === 'T') {
+      text.innerHTML = 'STALEMATE';
+    } else if (winner) {
+      text.innerHTML = `WINNER WINNER CHICKEN DINNER CONGRATS <span style="color: ${colors[winner]}">${colors[winner].toUpperCase()}</span>!`;
+    } else {
+      text.innerHTML = `<span style="color: ${colors[playersTurn]}">${colors[playersTurn].toUpperCase()}</span>'s Turn`;
+    }
+  }
+  
